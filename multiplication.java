5dac5e0a8993fcea153073117df6d340bfd3920f
Prajith-97 <tprajith70@gmail.com>
2023-10-11 09:51:07 +0530

    first commit

---
+++ multiplication.java
@@ -0,0 +1,17 @@
+package practiceDay1;
+
+public class multiplication {
+    public void multiplyTwoNumbers(float num1,float num2) {
+        try {
+            float mul = num1 * num2;
+            System.out.println(mul);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args){
+            multiplication obj = new multiplication();
+            obj.multiplyTwoNumbers(1.0f, Float.parseFloat("abc"));
+        }
+
+}
